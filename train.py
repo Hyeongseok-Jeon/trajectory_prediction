2316fcaa19c5619e5bbf4358ef2cc89a592cd0c7
Hyeongseok Jeon <hsjeon@morai.ai>
2022-08-24 17:03:26 +0900

    'update'

--- train.py
+++ train.py
@@ -20,21 +20,15 @@ from numbers import Number
 from tqdm import tqdm
 import torch
 from torch.utils.data import Sampler, DataLoader
-import horovod.torch as hvd
 
 
 from torch.utils.data.distributed import DistributedSampler
 
 from utils import Logger, load_pretrain
 
-from mpi4py import MPI
 
-
-comm = MPI.COMM_WORLD
-hvd.init()
-torch.cuda.set_device(hvd.local_rank())
-
-root_path = os.path.dirname(os.path.abspath(__file__))
+# root_path = os.path.dirname(os.path.abspath(__file__))
+root_path = "D:\\research\\trajectory_prediction"
 sys.path.insert(0, root_path)
 
 
@@ -49,24 +43,15 @@ parser.add_argument(
 parser.add_argument(
     "--weight", default="", type=str, metavar="WEIGHT", help="checkpoint path"
 )
-
+parser.add_argument(
+    "--port", default=3949, type=str, metavar="WEIGHT", help="checkpoint path"
+)
 
 def main():
-    seed = hvd.rank()
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-
     # Import all settings for experiment.
     args = parser.parse_args()
     model = import_module(args.model)
     config, Dataset, collate_fn, net, loss, post_process, opt = model.get_model()
-
-    if config["horovod"]:
-        opt.opt = hvd.DistributedOptimizer(
-            opt.opt, named_parameters=net.named_parameters()
-        )
 
     if args.resume or args.weight:
         ckpt_path = args.resume or args.weight
@@ -81,69 +66,56 @@ def main():
     if args.eval:
         # Data loader for evaluation
         dataset = Dataset(config["val_split"], config, train=False)
-        val_sampler = DistributedSampler(
-            dataset, num_replicas=hvd.size(), rank=hvd.rank()
-        )
+
         val_loader = DataLoader(
             dataset,
             batch_size=config["val_batch_size"],
             num_workers=config["val_workers"],
-            sampler=val_sampler,
             collate_fn=collate_fn,
             pin_memory=True,
         )
 
-        hvd.broadcast_parameters(net.state_dict(), root_rank=0)
         val(config, val_loader, net, loss, post_process, 999)
         return
 
     # Create log and copy all code
     save_dir = config["save_dir"]
     log = os.path.join(save_dir, "log")
-    if hvd.rank() == 0:
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-        sys.stdout = Logger(log)
 
-        src_dirs = [root_path]
-        dst_dirs = [os.path.join(save_dir, "files")]
-        for src_dir, dst_dir in zip(src_dirs, dst_dirs):
-            files = [f for f in os.listdir(src_dir) if f.endswith(".py")]
-            if not os.path.exists(dst_dir):
-                os.makedirs(dst_dir)
-            for f in files:
-                shutil.copy(os.path.join(src_dir, f), os.path.join(dst_dir, f))
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    sys.stdout = Logger(log)
+
+    src_dirs = [root_path]
+    dst_dirs = [os.path.join(save_dir, "files")]
+    for src_dir, dst_dir in zip(src_dirs, dst_dirs):
+        files = [f for f in os.listdir(src_dir) if f.endswith(".py")]
+        if not os.path.exists(dst_dir):
+            os.makedirs(dst_dir)
+        for f in files:
+            shutil.copy(os.path.join(src_dir, f), os.path.join(dst_dir, f))
 
     # Data loader for training
     dataset = Dataset(config["train_split"], config, train=True)
-    train_sampler = DistributedSampler(
-        dataset, num_replicas=hvd.size(), rank=hvd.rank()
-    )
     train_loader = DataLoader(
         dataset,
         batch_size=config["batch_size"],
         num_workers=config["workers"],
-        sampler=train_sampler,
         collate_fn=collate_fn,
         pin_memory=True,
-        worker_init_fn=worker_init_fn,
         drop_last=True,
     )
 
     # Data loader for evaluation
-    dataset = Dataset(config["val_split"], config, train=False)
-    val_sampler = DistributedSampler(dataset, num_replicas=hvd.size(), rank=hvd.rank())
+    dataset = Dataset(config["train_split"], config, train=True)
     val_loader = DataLoader(
         dataset,
         batch_size=config["val_batch_size"],
         num_workers=config["val_workers"],
-        sampler=val_sampler,
         collate_fn=collate_fn,
         pin_memory=True,
     )
 
-    hvd.broadcast_parameters(net.state_dict(), root_rank=0)
-    hvd.broadcast_optimizer_state(opt.opt, root_rank=0)
 
     epoch = config["epoch"]
     remaining_epochs = int(np.ceil(config["num_epochs"] - epoch))
@@ -151,28 +123,20 @@ def main():
         train(epoch + i, config, train_loader, net, loss, post_process, opt, val_loader)
 
 
-def worker_init_fn(pid):
-    np_seed = hvd.rank() * 1024 + int(pid)
-    np.random.seed(np_seed)
-    random_seed = np.random.randint(2 ** 32 - 1)
-    random.seed(random_seed)
-
-
 def train(epoch, config, train_loader, net, loss, post_process, opt, val_loader=None):
-    train_loader.sampler.set_epoch(int(epoch))
     net.train()
 
     num_batches = len(train_loader)
     epoch_per_batch = 1.0 / num_batches
     save_iters = int(np.ceil(config["save_freq"] * num_batches))
     display_iters = int(
-        config["display_iters"] / (hvd.size() * config["batch_size"])
+        config["display_iters"] / (config["batch_size"])
     )
-    val_iters = int(config["val_iters"] / (hvd.size() * config["batch_size"]))
+    val_iters = int(config["val_iters"] / (config["batch_size"]))
 
     start_time = time.time()
     metrics = dict()
-    for i, data in tqdm(enumerate(train_loader),disable=hvd.rank()):
+    for i, data in tqdm(enumerate(train_loader)):
         epoch += epoch_per_batch
         data = dict(data)
 
@@ -186,16 +150,13 @@ def train(epoch, config, train_loader, net, loss, post_process, opt, val_loader=
         lr = opt.step(epoch)
 
         num_iters = int(np.round(epoch * num_batches))
-        if hvd.rank() == 0 and (
-            num_iters % save_iters == 0 or epoch >= config["num_epochs"]
-        ):
+        if num_iters % save_iters == 0 or epoch >= config["num_epochs"]:
             save_ckpt(net, opt, config["save_dir"], epoch)
 
         if num_iters % display_iters == 0:
             dt = time.time() - start_time
             metrics = sync(metrics)
-            if hvd.rank() == 0:
-                post_process.display(metrics, dt, epoch, lr)
+            post_process.display(metrics, dt, epoch, lr)
             start_time = time.time()
             metrics = dict()
 
