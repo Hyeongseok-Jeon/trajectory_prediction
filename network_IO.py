57513e696f807a43fd2754d0293eea1e8785178f
Hyeongseok Jeon <hsjeon@morai.ai>
2022-09-29 21:51:59 +0900

    'sadf'

--- network_IO.py
+++ network_IO.py
@@ -14,7 +14,7 @@ import os.path
 from os import path
 from pandas import Series, DataFrame
 
-MAX_PIPE_BUFFER_SIZE = 64*1024*1024
+MAX_PIPE_BUFFER_SIZE = 64 * 1024 * 1024
 IN_PIPE_NAME = "\\\\.\\pipe\\python_in"
 OUT_PIPE_NAME = "\\\\.\\pipe\\c_in"
 
@@ -27,50 +27,70 @@ parser.add_argument("--port", default=52162)
 config = parser.parse_args()
 myProj = Proj(proj='utm', zone=52, ellps='WGS84', datum='WGS84', units='m')
 
-raw_data = {'TIMESTAMP': [],
-            'TRACK_ID': [],
-            'OBJECT_TYPE': [],
-            'X' : [],
-            'Y' : [],
-            'CITY_NAME' : []}
+def update_mem(mem, new):
+    time_list = mem['TIMESTAMP'].values.tolist()
+    time_list_sort = []
+    [time_list_sort.append(x) for x in time_list if x not in time_list_sort]
+    print(len(time_list_sort))
+    if len(time_list_sort) == 80:
+        print('save')
+        mem.to_csv('D://research//trajectory_prediction//dataset', index_label=Falsearer)
+        raw_data = {'TIMESTAMP': [],
+                    'TRACK_ID': [],
+                    'OBJECT_TYPE': [],
+                    'X': [],
+                    'Y': [],
+                    'CITY_NAME': [],
+                    'HEADING': []}
+        new_mem = DataFrame(raw_data)
+    else:
+        new_mem = pd.concat([mem, new])
 
-data = DataFrame(raw_data)
 
-def update_mem(obj_mem):
 
-    return obj_mem
+    return new_mem
+
 
 def pipe_server():
-    #region Config
+    # region Config
     pipe_buffer_size = ctypes.sizeof(message_t)
     msg = message_t()
     msg.id = MESSAGE_OK
     message_type = MESSAGE_OK
 
-    #endregion
+    # endregion
     pipe = Pipes(IN_PIPE_NAME, OUT_PIPE_NAME, MAX_PIPE_BUFFER_SIZE, connect_first=False)
     print("pipe server")
-    veh_list = []
+
+    raw_data = {'TIMESTAMP': [],
+                'TRACK_ID': [],
+                'OBJECT_TYPE': [],
+                'X': [],
+                'Y': [],
+                'CITY_NAME': [],
+                'HEADING': []}
+    mem = DataFrame(raw_data)
+    print(mem)
 
     while True:
         status, resp = pipe.read_pipe(pipe_buffer_size)
-        if status==0:
+        if status == 0:
             print("Status:", status)
-            if message_type==MESSAGE_OK:
+            if message_type == MESSAGE_OK:
                 # Read message
                 msg = message_t.from_buffer_copy(resp)
-                print("Message: %d"%msg.id)
+                print("Message: %d" % msg.id)
                 pipe_buffer_size = msg.size
                 message_type = msg.id
 
-            elif message_type==MESSAGE_OBJ:
+            elif message_type == MESSAGE_OBJ:
                 print("OBJ received")
-                num_obj = int(len(resp)/44)
+                num_obj = int(len(resp) / 44)
                 print(num_obj)
 
                 obj = dict()
                 for i in range(num_obj):
-                    data = resp[44*i:44*i+44]
+                    data = resp[44 * i:44 * i + 44]
                     if i == 0:
                         obj['time_stamp'] = int.from_bytes(data[0:4], 'little', signed=False)
                         obj['obj_num'] = num_obj
@@ -101,15 +121,17 @@ def pipe_server():
                         obj_info['obj_width'] = struct.unpack('<f', data[36:40])[0]
                         obj_info['obj_length'] = struct.unpack('<f', data[40:44])[0]
                         obj['obj_infos'].append(obj_info)
-                # print(['asdfasdfasdfasdfasdf', cur_time, veh_list[0]])
-                if len(veh_list) > 0:
-                    cur_time = veh_list[0][0]
-                    id_mask = '000-0000-0000'
-                    obj_id = id_mask[:-len(str(obj_info['obj_id']))] + str(obj_info['obj_id'])
-                    ##TODO : 좌표 변환
-                    veh_list.append([cur_time, obj_id, 'OTHERS', obj_info['rel_pos_lat'], obj_info['rel_pos_long'], 'HMC',obj_info['heading_angle']])
-                data_temp = pd.DataFrame(veh_list, columns=['TIMESTAMP','TRACK_ID','OBJECT_TYPE','X','Y','CITY_NAME','HEADING'])
-                print(data_temp)
+                    # print(['asdfasdfasdfasdfasdf', cur_time, veh_list[0]])
+                    if len(veh_list) > 0 and obj_info['obj_id'] != 0:
+                        cur_time = veh_list[0][0]
+                        id_mask = '000-0000-0000'
+                        obj_id = id_mask[:-len(str(obj_info['obj_id']))] + str(obj_info['obj_id'])
+                        ##TODO : 좌표 변환
+                        veh_list.append([cur_time, obj_id, 'OTHERS', obj_info['rel_pos_lat'], obj_info['rel_pos_long'], 'HMC', obj_info['heading_angle']])
+                    data_temp = pd.DataFrame(veh_list, columns=['TIMESTAMP', 'TRACK_ID', 'OBJECT_TYPE', 'X', 'Y', 'CITY_NAME', 'HEADING'])
+                mem = update_mem(mem, data_temp)
+                print(mem)
+
                 file_name = 'saving/objs/' + str(cur_time) + '.pkl'
                 with open(file_name, 'wb') as f:
                     pickle.dump(obj, f)
@@ -122,7 +144,9 @@ def pipe_server():
 
                 # Set message type to OK
                 message_type = MESSAGE_OK
-            elif message_type==MESSAGE_EGO:
+            elif message_type == MESSAGE_EGO:
+                veh_list = []
+
                 print("EGO received")
 
                 ego = dict()
@@ -133,8 +157,8 @@ def pipe_server():
                 ego['long_deg'] = struct.unpack('<d', resp[24:32])[0]
                 ego['heading'] = struct.unpack('<f', resp[32:36])[0]
                 ego['X'], ego['Y'] = myProj(ego['long_deg'], ego['lat_deg'])
-                if len(veh_list)==0:
-                    veh_list.append([ego['time_stamp'], '000-0000-0000', 'AV', ego['X'], ego['Y'], 'HMC',ego['heading']])
+                if len(veh_list) == 0:
+                    veh_list.append([ego['time_stamp'], '000-0000-0000', 'AV', ego['X'], ego['Y'], 'HMC', ego['heading']])
                 cur_time = ego['time_stamp']
 
                 file_name = 'saving/ego/' + str(cur_time) + '.pkl'
@@ -150,24 +174,24 @@ def pipe_server():
                 # Set message type to OK
                 message_type = MESSAGE_OK
 
-            elif message_type==MESSAGE_MAP:
+            elif message_type == MESSAGE_MAP:
                 # with open("map.data", "wb") as f:
                 #     f.write(resp)
 
                 print("Map received")
-                num_lane_links = int(len(resp)/9024)
+                num_lane_links = int(len(resp) / 9024)
                 for i in range(num_lane_links):
-                    data = resp[9024*i:9024*i+9024]
+                    data = resp[9024 * i:9024 * i + 9024]
                     lane_link = dict()
                     lane_link['next_ids_count'] = int.from_bytes(data[0:4], 'little', signed=False)
                     lane_link['prev_ids_count'] = int.from_bytes(data[88:92], 'little', signed=False)
                     lane_link['next_id'] = []
                     lane_link['prev_id'] = []
                     for num_cnt in range(10):
-                        lane_link['next_id'].append(int.from_bytes(data[8*num_cnt+8:8*num_cnt+16], 'little', signed=False))
+                        lane_link['next_id'].append(int.from_bytes(data[8 * num_cnt + 8:8 * num_cnt + 16], 'little', signed=False))
                         lane_link['prev_id'].append(int.from_bytes(data[8 * num_cnt + 96:8 * num_cnt + 104], 'little', signed=False))
                     lane_link['ref_lat'] = struct.unpack('<d', data[176:184])[0]
-                    lane_link['ref_lng'] = struct.unpack('<d',data[184:192])[0]
+                    lane_link['ref_lng'] = struct.unpack('<d', data[184:192])[0]
                     ref_utm_x, ref_utm_y = myProj(lane_link['ref_lng'], lane_link['ref_lat'])
 
                     lane_link['points_count'] = int.from_bytes(data[192:196], 'little', signed=False)
@@ -178,14 +202,14 @@ def pipe_server():
                     for num_cnt in range(550):
                         lane_link['points_x'].append(struct.unpack('<d', data[8 * num_cnt + 200:8 * num_cnt + 208])[0])
                         lane_link['points_y'].append(struct.unpack('<d', data[8 * num_cnt + 4600:8 * num_cnt + 4608])[0])
-                        lane_link['points_x_utm'].append(struct.unpack('<d', data[8*num_cnt+200:8*num_cnt+208])[0]+ref_utm_x)
-                        lane_link['points_y_utm'].append(struct.unpack('<d', data[8*num_cnt+4600:8*num_cnt+4608])[0]+ref_utm_y)
-                    lane_link['id']=int.from_bytes(data[9000:9008], 'little', signed=False)
+                        lane_link['points_x_utm'].append(struct.unpack('<d', data[8 * num_cnt + 200:8 * num_cnt + 208])[0] + ref_utm_x)
+                        lane_link['points_y_utm'].append(struct.unpack('<d', data[8 * num_cnt + 4600:8 * num_cnt + 4608])[0] + ref_utm_y)
+                    lane_link['id'] = int.from_bytes(data[9000:9008], 'little', signed=False)
                     lane_link['left_id'] = int.from_bytes(data[9008:9016], 'little', signed=False)
                     lane_link['right_id'] = int.from_bytes(data[9016:9024], 'little', signed=False)
 
                     file_name = 'saving/map/' + str(lane_link['id']) + '.pkl'
-                    if path.exists(file_name)==True:
+                    if path.exists(file_name) == True:
                         pass
                     else:
                         with open(file_name, 'wb') as f:
@@ -200,7 +224,6 @@ def pipe_server():
 
                 # Set message type to OK
                 message_type = MESSAGE_OK
-
 
     print("finished now")
 
