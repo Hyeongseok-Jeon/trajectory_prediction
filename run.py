779eeda78c33b73b70ad20d4ce05da08b0c86bbe
Hyeongseok Jeon <hsjeon@morai.ai>
2022-12-02 00:22:08 +0900

    'asdf'

--- run.py
+++ run.py
@@ -16,9 +16,15 @@ from pandas import Series, DataFrame
 from utils import read_argo_data, get_obj_feats, get_lane_graph
 from argoverse_api.argoverse.map_representation.map_api import ArgoverseMap
 import time
+import torch
+from data import ArgoDataset as Dataset, from_numpy, ref_copy, collate_fn
+from torch.utils.data import DataLoader
+from utils import Logger, load_pretrain, gpu
+
 
 from xml.etree.ElementTree import Element, SubElement, ElementTree, dump
 import json
+pd.set_option('display.max_rows', None)
 
 MAX_PIPE_BUFFER_SIZE = 64 * 1024 * 1024
 IN_PIPE_NAME = "\\\\.\\pipe\\python_in"
@@ -36,7 +42,13 @@ from importlib import import_module
 
 model = import_module('lanegcn')
 config, Dataset, collate_fn, net, loss, post_process, opt = model.get_model()
-
+config["preprocess"] = False  # we use raw data to generate preprocess data
+config["val_workers"] = 16
+config["workers"] = 16
+config['cross_dist'] = 6
+config['cross_angle'] = 0.5 * np.pi
+config["batch_size"] = 1
+config["val_batch_size"] = 1
 def indent(elem, level=0):
     i = "\n" + level * "  "
     if len(elem):
@@ -53,6 +65,190 @@ def indent(elem, level=0):
             elem.tail = i
 
 
+def to_numpy(data):
+    """Recursively transform torch.Tensor to numpy.ndarray.
+    """
+    if isinstance(data, dict):
+        for key in data.keys():
+            data[key] = to_numpy(data[key])
+    if isinstance(data, list) or isinstance(data, tuple):
+        data = [to_numpy(x) for x in data]
+    if torch.is_tensor(data):
+        data = data.numpy()
+    return data
+
+
+def to_int16(data):
+    if isinstance(data, dict):
+        for key in data.keys():
+            data[key] = to_int16(data[key])
+    if isinstance(data, list) or isinstance(data, tuple):
+        data = [to_int16(x) for x in data]
+    if isinstance(data, np.ndarray) and data.dtype == np.int64:
+        data = data.astype(np.int16)
+    return data
+
+
+class PreprocessDataset():
+    def __init__(self, split, config, train=True):
+        self.split = split
+        self.config = config
+        self.train = train
+
+    def __getitem__(self, idx):
+        from data import from_numpy, ref_copy
+
+        data = self.split[idx]
+        graph = dict()
+        for key in ['lane_idcs', 'ctrs', 'pre_pairs', 'suc_pairs', 'left_pairs', 'right_pairs', 'feats']:
+            graph[key] = ref_copy(data['graph'][key])
+        graph['idx'] = idx
+        return graph
+
+    def __len__(self):
+        return len(self.split)
+
+
+def preprocess(graph, cross_dist, cross_angle=None):
+    left, right = dict(), dict()
+
+    lane_idcs = graph['lane_idcs']
+    num_nodes = len(lane_idcs)
+    num_lanes = lane_idcs[-1].item() + 1
+
+    dist = graph['ctrs'].unsqueeze(1) - graph['ctrs'].unsqueeze(0)
+    dist = torch.sqrt((dist ** 2).sum(2))
+    hi = torch.arange(num_nodes).long().to(dist.device).view(-1, 1).repeat(1, num_nodes).view(-1)
+    wi = torch.arange(num_nodes).long().to(dist.device).view(1, -1).repeat(num_nodes, 1).view(-1)
+    row_idcs = torch.arange(num_nodes).long().to(dist.device)
+
+    if cross_angle is not None:
+        f1 = graph['feats'][hi]
+        f2 = graph['ctrs'][wi] - graph['ctrs'][hi]
+        t1 = torch.atan2(f1[:, 1], f1[:, 0])
+        t2 = torch.atan2(f2[:, 1], f2[:, 0])
+        dt = t2 - t1
+        m = dt > 2 * np.pi
+        dt[m] = dt[m] - 2 * np.pi
+        m = dt < -2 * np.pi
+        dt[m] = dt[m] + 2 * np.pi
+        mask = torch.logical_and(dt > 0, dt < config['cross_angle'])
+        left_mask = mask.logical_not()
+        mask = torch.logical_and(dt < 0, dt > -config['cross_angle'])
+        right_mask = mask.logical_not()
+
+    pre = graph['pre_pairs'].new().float().resize_(num_lanes, num_lanes).zero_()
+    pre[graph['pre_pairs'][:, 0], graph['pre_pairs'][:, 1]] = 1
+    suc = graph['suc_pairs'].new().float().resize_(num_lanes, num_lanes).zero_()
+    suc[graph['suc_pairs'][:, 0], graph['suc_pairs'][:, 1]] = 1
+
+    pairs = graph['left_pairs']
+    if len(pairs) > 0:
+        mat = pairs.new().float().resize_(num_lanes, num_lanes).zero_()
+        mat[pairs[:, 0], pairs[:, 1]] = 1
+        mat = (torch.matmul(mat, pre) + torch.matmul(mat, suc) + mat) > 0.5
+
+        left_dist = dist.clone()
+        mask = mat[lane_idcs[hi], lane_idcs[wi]].logical_not()
+        left_dist[hi[mask], wi[mask]] = 1e6
+        if cross_angle is not None:
+            left_dist[hi[left_mask], wi[left_mask]] = 1e6
+
+        min_dist, min_idcs = left_dist.min(1)
+        mask = min_dist < cross_dist
+        ui = row_idcs[mask]
+        vi = min_idcs[mask]
+        f1 = graph['feats'][ui]
+        f2 = graph['feats'][vi]
+        t1 = torch.atan2(f1[:, 1], f1[:, 0])
+        t2 = torch.atan2(f2[:, 1], f2[:, 0])
+        dt = torch.abs(t1 - t2)
+        m = dt > np.pi
+        dt[m] = torch.abs(dt[m] - 2 * np.pi)
+        m = dt < 0.25 * np.pi
+
+        ui = ui[m]
+        vi = vi[m]
+
+        left['u'] = ui.cpu().numpy().astype(np.int16)
+        left['v'] = vi.cpu().numpy().astype(np.int16)
+    else:
+        left['u'] = np.zeros(0, np.int16)
+        left['v'] = np.zeros(0, np.int16)
+
+    pairs = graph['right_pairs']
+    if len(pairs) > 0:
+        mat = pairs.new().float().resize_(num_lanes, num_lanes).zero_()
+        mat[pairs[:, 0], pairs[:, 1]] = 1
+        mat = (torch.matmul(mat, pre) + torch.matmul(mat, suc) + mat) > 0.5
+
+        right_dist = dist.clone()
+        mask = mat[lane_idcs[hi], lane_idcs[wi]].logical_not()
+        right_dist[hi[mask], wi[mask]] = 1e6
+        if cross_angle is not None:
+            right_dist[hi[right_mask], wi[right_mask]] = 1e6
+
+        min_dist, min_idcs = right_dist.min(1)
+        mask = min_dist < cross_dist
+        ui = row_idcs[mask]
+        vi = min_idcs[mask]
+        f1 = graph['feats'][ui]
+        f2 = graph['feats'][vi]
+        t1 = torch.atan2(f1[:, 1], f1[:, 0])
+        t2 = torch.atan2(f2[:, 1], f2[:, 0])
+        dt = torch.abs(t1 - t2)
+        m = dt > np.pi
+        dt[m] = torch.abs(dt[m] - 2 * np.pi)
+        m = dt < 0.25 * np.pi
+
+        ui = ui[m]
+        vi = vi[m]
+
+        right['u'] = ui.cpu().numpy().astype(np.int16)
+        right['v'] = vi.cpu().numpy().astype(np.int16)
+    else:
+        right['u'] = np.zeros(0, np.int16)
+        right['v'] = np.zeros(0, np.int16)
+
+    out = dict()
+    out['left'] = left
+    out['right'] = right
+    out['idx'] = graph['idx']
+    return out
+
+
+def to_long(data):
+    if isinstance(data, dict):
+        for key in data.keys():
+            data[key] = to_long(data[key])
+    if isinstance(data, list) or isinstance(data, tuple):
+        data = [to_long(x) for x in data]
+    if torch.is_tensor(data) and data.dtype == torch.int16:
+        data = data.long()
+    return data
+
+
+def modify(config, data_loader, save):
+    t = time.time()
+    store = data_loader.dataset.split
+    print('7')
+
+    for i, data in enumerate(data_loader):
+        print('8')
+
+        data = [dict(x) for x in data]
+
+        out = []
+        for j in range(len(data)):
+            out.append(preprocess(to_long(gpu(data[j])), config['cross_dist']))
+        print('9')
+
+        for j, graph in enumerate(out):
+            idx = graph['idx']
+            store[idx]['graph']['left'] = graph['left']
+            store[idx]['graph']['right'] = graph['right']
+    return store
+
 def update_mem(mem, new, am, fov):
     time_list = mem['TIMESTAMP'].values.tolist()
     time_list_sort = []
@@ -67,47 +263,61 @@ def update_mem(mem, new, am, fov):
                     full_id.append(id_list[i])
         full_id.remove('000-0000-0000')
         init = time.time()
-        data_tot = {'city': [],
-                    'orig': [],
-                    'gt_preds': [],
-                    'has_preds': [],
-                    'theta': [],
-                    'rot': [],
-                    'feats': [],
-                    'ctrs': [],
-                    'graph': []}
+
+        data_tot = [None for x in range(len(full_id))]
+        print('4')
+
         for i in range(len(full_id)):
             data_save = mem.copy()
 
             indices = [j for j, x in enumerate(list(data_save['TRACK_ID'])) if x == full_id[i]]
             for k in indices:
                 data_save.iat[k, 2] = 'AGENT'
-
-            print(data_input['feats'])
             data_input = read_argo_data('HMC',data_save)
-            print('1', data_input['feats'])
             data_input = get_obj_feats(data_input)
-            print('2', data_input['feats'])
             data_input['graph'] = get_lane_graph(data_input, am)
-            print('3', data_input['feats'])
+            data_input['idx'] = i
 
-            data_tot['city'].append(data_input['city'])
-            data_tot['orig'].append(data_input['orig'])
-            data_tot['gt_preds'].append(data_input['gt_preds'])
-            data_tot['has_preds'].append(data_input['has_preds'])
-            data_tot['theta'].append(data_input['theta'])
-            data_tot['rot'].append(data_input['rot'])
-            data_tot['feats'].append(data_input['feats'])
-            data_tot['ctrs'].append(data_input['ctrs'])
-            data_tot['graph'].append(data_input['graph'])
-        print(data_tot["feats"])
+            store = dict()
+            for key in [
+                "idx",
+                "city",
+                "feats",
+                "ctrs",
+                "orig",
+                "theta",
+                "rot",
+                "gt_preds",
+                "has_preds",
+                "graph",
+            ]:
+                store[key] = to_numpy(data_input[key])
+                if key in ["graph"]:
+                    store[key] = to_int16(store[key])
+            data_tot[store["idx"]] = store
+        print('5')
+
+        dataset = PreprocessDataset(data_tot, config, train=True)
+
+        data_loader = DataLoader(
+            dataset,
+            batch_size=config['batch_size'],
+            num_workers=config['workers'],
+            shuffle=False,
+            collate_fn=from_numpy,
+            pin_memory=True,
+            drop_last=False)
+
+        data = modify(config, data_loader, config["preprocess_train"])
+
+        # print(data_tot["feats"])
         output = net(data_tot)
 
         print('conversion finished', time.time()-init)
 
         # new_mem = DataFrame(raw_data)
     else:
-        new_mem = pd.concat([mem, new])
+        new_mem = pd.concat([mem, new], ignore_index=True)
 
 
     return new_mem
